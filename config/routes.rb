66de46065a53254045ea3dbecd44affdec707ded
coolJenny <coolJenny009g@gmail.com>
2018-05-30 01:22:28 -0400

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   resources :flashcards do
   	get :tags, on: :member
   end
-  
+
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/auth/github', to: 'authentication#github', format: false
 end
